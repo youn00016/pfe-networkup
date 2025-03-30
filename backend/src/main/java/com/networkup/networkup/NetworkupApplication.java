e35e59194202fdf824c5cf782ac25da43285bbd9
younes <elalaouiyounes68@gmail.com>
2025-03-30 17:26:24 +0000

    chore :set up networkup

---
+++ backend/src/main/java/com/networkup/networkup/NetworkupApplication.java
@@ -0,0 +1,13 @@
+package com.networkup.networkup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkupApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/networkup/networkup/NetworkupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.networkup.networkup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
